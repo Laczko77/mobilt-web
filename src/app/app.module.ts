1a97152fbc5578d59f6aad35c34d79ff3aa46d94
Laczko77 <kristof.laczkovich@gmail.com>
2024-05-17 18:29:07 +0200

    AHAM

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { MainComponent } from './src/pages/main/main.component';
+import { LoginComponent } from './pages/login/login/login.component';
+import { SignupComponent } from './pages/signup/signup/signup.component';
+import { MarketComponent } from './pages/market/market/market.component';
+import { CartComponent } from './pages/cart/cart/cart.component';
+import { ProfileComponent } from './pages/profile/profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    LoginComponent,
+    SignupComponent,
+    MarketComponent,
+    CartComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration(),
+    provideFirebaseApp(() => initializeApp({"projectId":"mobiltweb-32764","appId":"1:823367235461:web:9c38f4ae9bfcfb863736ec","storageBucket":"mobiltweb-32764.appspot.com","apiKey":"AIzaSyDBrniPnVzqSJqFYvtZVWW1565_Eq5otHg","authDomain":"mobiltweb-32764.firebaseapp.com","messagingSenderId":"823367235461","measurementId":"G-Q58HHNNTRR"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment= {
+    firebase:{
+        apiKey: "AIzaSyDBrniPnVzqSJqFYvtZVWW1565_Eq5otHg",
+        authDomain: "mobiltweb-32764.firebaseapp.com",
+        projectId: "mobiltweb-32764",
+        storageBucket: "mobiltweb-32764.appspot.com",
+        messagingSenderId: "823367235461",
+        appId: "1:823367235461:web:9c38f4ae9bfcfb863736ec",
+        measurementId: "G-Q58HHNNTRR"
+    },
+    production: false,
+    
+};
